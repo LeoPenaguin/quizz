cb8f84a12881ce74987ace886ec545d46ea057f9
leo-penaguin <penaguinleo@gmail.com>
2022-07-31 20:22:51 +0200

    Add server

--- packages/socket/index.ts
+++ packages/socket/index.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io();
+const socket = io("http://localhost:3000");
 
 socket.on("counter", function (count) {
   console.log(count);
--- server/index.ts
+++ server/index.ts
@@ -1,19 +1,22 @@
-import express from "express";
-import { createServer } from "http";
-import { Server } from "socket.io"; //replaces (import socketIo from 'socket.io')
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, { cors: { origin: "*" } });
+const io = new Server(httpServer, { cors: { origin: '*' } });
 
-app.get("/json", function (req, res) {
-   res.status(200).json({ message: "ok" });
+app.use(cors());
+
+app.get('/json', (req, res) => {
+  res.status(200).json({ message: 'ok' });
 });
 
-io.on("connection", (socket) => {
+io.on('connection', (socket) => {
   console.log(`Connecté au client ${socket.id}`);
 });
 
-httpServer.listen(3000, function () {
-  console.log("Votre app est disponible sur localhost:3000 !");
+httpServer.listen(3000, () => {
+  console.log('Votre app est disponible sur localhost:3000 !');
 });
